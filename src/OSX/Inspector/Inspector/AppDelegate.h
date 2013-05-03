4faa3a88a21e094dea27561e7517cd21633be2cf
ultragtx <ultragtx@gmail.com>
2013-05-03 21:36:37 +0800

    OSX Initial Commit. Add OBMenuBarWindow (3rd party)

---
+++ src/OSX/Inspector/Inspector/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Inspector
+//
+//  Created by Xinrong Guo on 13-5-3.
+//  Copyright (c) 2013年 Xinrong Guo. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ src/OSX/Inspector/Inspector/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  Inspector
+//
+//  Created by Xinrong Guo on 13-5-3.
+//  Copyright (c) 2013年 Xinrong Guo. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
