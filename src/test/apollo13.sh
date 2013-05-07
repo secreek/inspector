20151b30aa3c4277e72725ad8c048c5a6c43ea52
zhf <hf@zhanghongfeng.com>
2013-05-07 14:39:22 +0800

    add dummy service and config for test

---
+++ src/test/apollo13.sh
@@ -0,0 +1,30 @@
+LOG=/tmp/inspector-test.log
+while true
+do
+	echo "JFK: \"We are going to the moon!\"" >> $LOG
+	sleep 3
+	echo "Launching Apollo 13 ..." >> $LOG
+	sleep 3
+	echo "Reaching Earth orbit" >> $LOG
+	sleep 3
+	echo "Everything is okay" >> $LOG
+	sleep 3
+	echo "On my way to the Moon" >> $LOG
+	sleep 3
+	echo "Camera rolling ... nice views" >> $LOG
+	sleep 3
+	echo "Error: Houston, We have Got a Problem" >> $LOG
+	sleep 3
+	echo "Error: We have had a MAIN B BUS UNDERVOLT" >> $LOG
+	sleep 3
+	echo "Error: Starting to go ahead and button up the tunnel again" >> $LOG
+	sleep 3
+	echo "Error: RESTART and a PGNCS light. RESET on a PGNCS, RESET" >> $LOG
+	sleep 3
+	echo "On the way home back to Earth" >> $LOG
+	sleep 3
+	echo "Parachutes opened and the Command Module splashed down into the South Pacific" >> $LOG
+	sleep 3
+	echo "Lovell: \"A successful failure!\"" >> $LOG
+	sleep 10
+done
